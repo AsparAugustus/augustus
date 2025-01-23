d950f9b471acf7d501dddf9496c630ca7ee5bc6c
Merlin <xlysergia@gmail.com>
2025-01-23 17:44:54 +0000

    new

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // If using the app directory structure
-  },
-  typescript: {
-    ignoreBuildErrors: false, // Ensure TypeScript errors are caught during the build
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
